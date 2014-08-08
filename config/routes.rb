872187d802c5c7fe801790e4eee64bb748caf0c2
Your Name <you@example.com>
2014-08-08 14:13:37 +0000

    Fixed creating user problem'

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,4 @@
 Rails.application.routes.draw do
-  # Routes for the Collection_panel resource:
-  # CREATE
-  get '/new_collection_panel' => 'collection_panels#new'
-  get '/create_collection_panel' => 'collection_panels#create'
-
-  # READ
-  get '/collection_panels' => 'collection_panels#index'
-  get '/collection_panels/:id' => 'collection_panels#show'
-
-  # UPDATE
-  get '/collection_panels/:id/edit' => 'collection_panels#edit'
-  get '/collection_panels/:id/update' => 'collection_panels#update'
-
-  # DELETE
-  get '/collection_panels/:id/destroy' => 'collection_panels#destroy'
   #------------------------------
 
   # Routes for the User_collection resource:
@@ -31,23 +16,7 @@ Rails.application.routes.draw do
 
   # DELETE
   get '/user_collections/:id/destroy' => 'user_collections#destroy'
-  #------------------------------
 
-  # Routes for the Website_collection resource:
-  # CREATE
-  get '/new_website_collection' => 'website_collections#new'
-  get '/create_website_collection' => 'website_collections#create'
-
-  # READ
-  get '/website_collections' => 'website_collections#index'
-  get '/website_collections/:id' => 'website_collections#show'
-
-  # UPDATE
-  get '/website_collections/:id/edit' => 'website_collections#edit'
-  get '/website_collections/:id/update' => 'website_collections#update'
-
-  # DELETE
-  get '/website_collections/:id/destroy' => 'website_collections#destroy'
   #------------------------------
 
   # Routes for the Collection resource:
@@ -68,22 +37,6 @@ Rails.application.routes.draw do
   get '/collections/:id/destroy' => 'collections#destroy'
   #------------------------------
 
-  # Routes for the Website resource:
-  # CREATE
-  get '/new_website' => 'websites#new'
-  get '/create_website' => 'websites#create'
-
-  # READ
-  get '/websites' => 'websites#index'
-  get '/websites/:id' => 'websites#show'
-
-  # UPDATE
-  get '/websites/:id/edit' => 'websites#edit'
-  get '/websites/:id/update' => 'websites#update'
-
-  # DELETE
-  get '/websites/:id/destroy' => 'websites#destroy'
-  #------------------------------
 
   # Routes for the User resource:
   # CREATE
